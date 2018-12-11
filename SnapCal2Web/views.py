5c15476a3daa0442a440d5969856a3355ba26e7e
Richard Guan <guanr@umich.edu>
2018-12-10 21:58:30 -0500

    imports and everything working

--- SnapCal2Web/views.py
+++ SnapCal2Web/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
+    # testing imports
+    from google.auth import app_engine
+    from google.cloud import vision
+    from google.cloud.vision import types
+
     return HttpResponse("Hello world! This is SnapCal2!")
