4fa15f63d925dcdf7ab0b985f3b7bc2bee1d8536
Richard Guan <guanr@umich.edu>
2019-01-08 21:49:55 -0500

    refactor(app) made sure that infinite redirecting wouldn't happen in feature

--- SnapCal2/urls.py
+++ SnapCal2/urls.py
@@ -14,10 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from SnapCal2Web import views
 
 urlpatterns = [
-    path('', include('SnapCal2Web.urls')),
-    path('admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^API/img_process', views.ProcessImageResponse.as_view(), name='process_image'),
+    url(r'admin', admin.site.urls),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- SnapCal2Web/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('API/img_process', views.ProcessImageResponse.as_view(), name='process_image'),
-]
